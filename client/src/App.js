669c7796383e50827cf6a3acaaf257613096f4ae
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2019-06-06 22:43:51 -0400

    heroku timeout issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -13,12 +12,12 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Wrapper>
+        <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-        </Wrapper>
+          <Route exact path="/contact" component={Contact} />
+          </Switch>
         <Footer/>
       </div>
     </Router>
