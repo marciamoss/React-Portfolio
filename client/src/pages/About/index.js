32121071560e4bda7f8f04316c26d1e086c9ba7d
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2021-02-06 15:53:19 -0500

    about me text update

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -22,13 +22,17 @@ function About() {
           </div>
           <div className="col-12 col-sm-12 col-md-7 col-lg-9 col-xl-9" style={{fontFamily: 'Times New Roman', color:"#000080"}}>
             <p>
-            <b><em>"Do the best you can until you know better. 
-              Then when you know better, do better."-- Maya Angelou</em><br/><br/></b>
-              That sums up in pretty much everything I do. Always striving for excellence and growth. 
-              Professionally a computer science graduate and a certified Full stack developer with main focus being on MERN stack 
-              (MongoDB, Express.js, React.js, Node.js). 
-              My strength lies in never giving up, good work ethic 
-              and a student for life which means I never get stagnant.
+            <b><em>Maya Angelo perfectly sums up the way I work in her quote, "Do the best you can until you know better.
+              Then when you know better, do better."</em><br/><br/></b>
+              I’m a full stack developer who is always striving for excellence and growth.
+              I specialize in JavaScript technologies across the MERN & MEAN stack (Mongo DB, Express.js, React.js, Angular and Node.js).
+              With experience in crafting clean front-ends and functional back-end API systems, I love to spend my time coding and,
+              due to my work ethic and focus, am known to deliver on time.
+              </p>
+              <p>
+                As a lifelong learner that is always looking to stay on top of leading technologies, I graduated from University of North Carolina’s
+                MERN Development Boot Camp and Farleigh Dickinson University with a Master in Computer Science.
+                As a result, I am savvy enough to build your next application from scratch, yet sophisticated enough to have developed Fortune 100 solutions.
               </p>
               <p style={{fontSize:"14px", color:"#000080"}} className="mb-5">
               <b>Education:<br/></b>  
@@ -39,17 +43,13 @@ function About() {
               </div>
               <b>Skills:</b><br/>
               <div className="ml-3">
-                Express, Security and Sessions storage, User Authentication, MERN-Stack MongoDB, Express.js,
-                React.js, Node.js<br/>
-                MySQL, ORM, Sequelize, firebase, MongoDB.<br/>
-                Heroku & Git<br/>
-                HTML5/CSS, Javascript/Jquery, Responsive design, Bootstrap, Handlebars, Firebase,
-                cookies/localstorage, React.js, API consumption, Json, Cloud database usage.<br/>
+                Languages: JavaScript, Typescript, HTML, CSS<br/>
+                Frameworks: Node.js, Express, Angular, React.js <br/>
+                Database Tools: SQL, Sequelize, PostgresSQL, Firebase, MongoDB, CouchDB<br/>
+                Other: Git/Github, jQuery, Bootstrap, Handlebars<br/>
                 SAS Certified Advanced Programmer for SAS 9 <em>(05JUL2017)</em><br/>
                 SAS Certified Base Programmer for SAS 9 <em>(15FEB2017)</em>
               </div>
-              
-                       
             </p>
           </div>
         </div>
